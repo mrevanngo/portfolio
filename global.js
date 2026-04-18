bdb0293c382eba39f6f97aa0591f137ffe019d9f
Evan Ngo <mr.evanngo@gmail.com>
2026-04-17 23:21:05 -0700

    nav menu

--- global.js
+++ global.js
@@ -4,10 +4,39 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                 // Local server
+  : "/portfolio/";      // GitHub Pages repo name
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname,
-);
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'resume/', title: 'Resume' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'https://github.com/mrevanngo', title: 'GitHub' },
+];
 
-currentLink?.classList.add('current');
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
+
+  if (url.startsWith('https://github.com')) {
+    a.target = '_blank';
+    a.rel = 'noopener noreferrer';
+  }
+
+  nav.append(a);
+}
