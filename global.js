885a5ee4d8f07fcc7d37b6c654bf344542d62eb5
Evan Ngo <mr.evanngo@gmail.com>
2026-04-24 16:42:34 -0700

    many edits

--- global.js
+++ global.js
@@ -1,5 +1,34 @@
 console.log('IT\'S ALIVE!');
 
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+  containerElement.innerHTML = '';
+
+  for (const project of projects) {
+    const article = document.createElement('article');
+    article.innerHTML = `
+      <${headingLevel}>${project.title}</${headingLevel}>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+    `;
+    containerElement.appendChild(article);
+  }
+}
+
+export async function fetchJSON(url) {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
