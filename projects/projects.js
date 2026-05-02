055d1a44f231e43c477bc25efe0c0151973ce68c
Evan Ngo <mr.evanngo@gmail.com>
2026-05-01 21:00:14 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -12,16 +12,16 @@ projectsTitle.textContent = `${projects.length} Projects`;
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-let data = [1, 2];
+let data = [1, 2, 3, 4, 5, 5];
 let sliceGenerator = d3.pie();
 let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
 
-let colors = ['gold', 'purple'];
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
 arcs.forEach((arc, idx) => {
   d3.select('svg')
     .append('path')
     .attr('d', arc)
-    .attr('fill', colors[idx]);
+    .attr('fill', colors(idx));
 });
